2d65b36bc679a29b81542e2a9bcf2915d31f65ad
azade95 <absalamzadeazade95@gmail.com>
2023-05-17 22:36:58 +0400

    added viewcomponent

---
+++ Pronia/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.DAL;
+
+namespace Pronia.ViewComponents
+{
+    public class HeaderViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeaderViewComponent(AppDbContext context)
+        {
+           _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Dictionary<string,string> settings=await _context.Settings.ToDictionaryAsync(s=>s.Key,s=>s.Value);
+            return View(settings);
+        }
+    }
+}
---
+++ Pronia/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pronia.DAL;
+using Pronia.Models;
+
+namespace Pronia.ViewComponents
+{
+    public class ProductViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ProductViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int key)
+        {
+            List<Product> products = new List<Product>();
+            switch (key)
+            {
+                case 1: products = _context.Products.OrderBy(p => p.Name).ToList(); break;
+                case 2: products = _context.Products.OrderBy(p => p.Price).ToList(); break;
+                case 3: products = _context.Products.OrderByDescending(p => p.Id).ToList(); break;
+                default:
+                    products = _context.Products.ToList();
+                    break;
+            }
+
+            //List<Product> products = _context.Products.OrderBy(p => p.Price).ToList();
+            return View(products);
+        }
+    }
+}
